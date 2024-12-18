3207ea5eb3032beae41e716e519b4c44f6829c0f
dmichaud03 <85fxllc@gmail.com>
2024-12-18 16:21:02 -0500

    Update route.ts

--- app/api/lessons/[lessonId]/route.ts
+++ app/api/lessons/[lessonId]/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 import { eq } from 'drizzle-orm';
-import  db  from '@/db/drizzle'
+import  db  from '@/db/drizzle';
 import { lessons } from '@/db/schema';
 import { params } from 'next/navigation';
 
